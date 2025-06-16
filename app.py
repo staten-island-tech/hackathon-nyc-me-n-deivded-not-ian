990dc56298993a71f96da07647db5fb6dab7a31e
Deivid-U <deividu@nycstudents.net>
2025-06-16 02:04:54 -0400

    pain
    there is nothing but pain in coding

--- app.py
+++ app.py
@@ -10,33 +10,37 @@ app.secret_key = os.urandom(32)
 
 @app.route('/')
 def index():
+    session.clear()
     return render_template("index.html")
 
 @app.route('/game', methods=['GET', 'POST'])
 def game():
     if "index" not in session:
         session["index"] = 0
-        session["landmarks"] = []
+        session["landmarks"] = {}
         session["correct_guess"] = False
         session["score"] = 0
         session["previous_landmark"] = {}
         session["current_landmark"] = {}
+        session["offsets"] = []
 
     if session.get("index") >= 25:
         return redirect('/endscreen')
     else:
         if request.method == "POST":
             borough_guess = request.form.get("guess").strip().upper()
-            if borough_guess == session["current_landmark"]["borough"]:
-                session["correct_guess"] = True
-                session["score"] += 1
-            else:
-                session["correct_guess"] = False
+            if "borough" in session.get("current_landmark"):
+                if borough_guess == session["current_landmark"]["borough"]:
+                    session["correct_guess"] = True
+                    session["score"] += 1
+                else:
+                    session["correct_guess"] = False
         
             session["index"] += 1
-            session["landmarks"].append(session["current_landmark"]["objectid"])
-            session["previous_landmark"] = session["current_landmark"]
-            session["current_landmark"] = {}
+            session["landmarks"][session["current_landmark"]["objectid"]] = {
+                "coords": [pair[::-1] for pair in session["current_landmark"]["the_geom"]["coordinates"][0][0]],
+                "name": session["current_landmark"]["lpc_name"]
+            }
 
             return redirect("/game")
 
@@ -44,7 +48,8 @@ def game():
         while True:
             offset = random.randint(0, 999)
             i += 1
-            if (offset + 1) not in session.get("landmarks"):
+            if offset not in session["offsets"]:
+                session["offsets"].append(offset)
                 break
             if i > 100:
                 return render_template("error.html", error = "Could not generate random landmark.")
@@ -59,43 +64,48 @@ def game():
         except ValueError as json_error:
             return render_template("error.html", error = f"Invalid JSON: {json_error}")
         else:
-            session["current_landmark"] = landmark[0]
+            try: 
+                landmark = landmark[0]
+                session["previous_landmark"] = session["current_landmark"]
+                session["current_landmark"] = {
+                  "objectid": landmark["objectid"],
+                  "the_geom": {
+                      "type": landmark["the_geom"]["type"],
+                      "coordinates": landmark["the_geom"]["coordinates"]
+                    },
+                  "address": landmark["address"],
+                  "lpc_name": landmark["lpc_name"],
+                  "borough": landmark["borough"],
+                  "url_report": landmark["url_report"]
+                }
+            except KeyError as error:
+                return redirect('/game')
+            else:
+                nyc_map = folium.Map(location=[40.7128, -74.0060], zoom_start=11)
 
-            nyc_map = folium.Map(location=[40.7128, -74.0060], zoom_start=13)
-            #loading every previous location on a map
-            for landmark_id in session["landmarks"]:
-                try:
-                    response = requests.get(f"https://data.cityofnewyork.us/resource/buis-pvji.json?$limit=1&$offset={landmark_id - 1}&$select=the_geom,lpc_name", timeout = 10)
-                    response.raise_for_status()       
-                    recalled_landmark = response.json()
-                except requests.exceptions.HTTPError as http_error:
-                    return render_template("error.html", error = f"HTTP Error: {http_error}")
-                except requests.exceptions.RequestException as request_error:
-                    return render_template("error.html", error = f"Request Error: {request_error}")
-                except ValueError as json_error:
-                    return render_template("error.html", error = f"Invalid JSON: {json_error}")
-                else:
-                    coords = [pair[::-1] for pair in recalled_landmark[0]["the_geom"]["coordinates"][0][0]]
-                    name = recalled_landmark[0]["lpc_name"]
+                if session["landmarks"]:
+                    for recalled_landmark in session["landmarks"].values():
+                        coords = recalled_landmark["coords"]
+                        name = recalled_landmark["name"]
 
-                    folium.Polygon(
-                        locations=coords,
-                        color='blue',
-                        fill=True,
-                        fill_color='lightblue',
-                        popup=name
-                    ).add_to(nyc_map)
+                        folium.Polygon(
+                            locations=coords,
+                            color='blue',
+                            fill=True,
+                            fill_color='lightblue',
+                            popup=name
+                        ).add_to(nyc_map)
 
-                    lat = sum([point[0] for point in coords]) / len(coords)
-                    lon = sum([point[1] for point in coords]) / len(coords)
+                        marker_lat = sum([point[0] for point in coords]) / len(coords)
+                        marker_lon = sum([point[1] for point in coords]) / len(coords)
 
-                    folium.Marker(
-                        location=[lat, lon],
-                        popup=name
-                    ).add_to(nyc_map)
+                        folium.Marker(
+                            location=[marker_lat, marker_lon],
+                            popup=name
+                        ).add_to(nyc_map)
 
-            folium_html = nyc_map._repr_html_()
-            return render_template("game.html", landmark=landmark, folium_html=folium_html, previous_landmark=session.get("previous_landmark"))
+                folium_html = nyc_map._repr_html_()
+                return render_template("game.html", landmark=landmark, folium_html=folium_html, previous_landmark=session.get("previous_landmark"), index=session.get("index"))
             
 @app.route('/endscreen')
 def endscreen():
